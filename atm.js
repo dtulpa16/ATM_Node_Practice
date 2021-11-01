5cd52d3026928ab6198ddc7f6adc9bd7b7a0753a
unknown <dtulpa16@yahoo.com>
2021-11-01 12:57:18 -0500

    Added functions for Depositing mondry into ATM + Validating Users Pin

--- atm.js
+++ atm.js
@@ -1,18 +1,26 @@
 "use strict";
 //TODO: Import necessary values from account.js
+import {balance , pin} from './account'
 
-function getBalance() {
+function getBalance(money) {
   //TODO: Return the customer's acct. balance
+  return alert(`Your current balance is ${money}`)
 }
 
 function withdraw(withdrawAmount) {
   //TODO: withdraw amount from current acct. balance
   // Log the current balance after withdrawal is made
+  balance -= withdrawAmount
+  alert(`Withdrawal successful! Your new balance is ${balance}`)
+  return balance
 }
 
 function deposit(depositAmount) {
   //TODO: deposit amount to current acct. balance
   // Log the current balance after deposit is made
+  balance -= withdrawAmount
+  alert(`Withdrawal successful! Your new balance is ${balance}`)
+  return balance
 }
 
 function validatePin(enteredPin) {
@@ -20,6 +28,18 @@ function validatePin(enteredPin) {
   //Allow access to ATM if matching
   //Return value should be a Boolean (true or false)
   //In English: if the PIN entered is correct, return True. Otherwise, return False.
+  if(enteredPin == pin){
+    return true
+  }else if(enteredPin != pin){
+    return false
+  }
 }
 
 //TODO: Export these functions
+
+module.exports={
+  balance: getBalance,
+  withdraw: withdraw,
+  deposit: deposit,
+  validatePin: validatePin
+}
